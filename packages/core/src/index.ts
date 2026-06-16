3109acb88a9396a6ac696ddade32cc7690c79f6e
ukimsanov <ular.kimsanov@heygen.com>
2026-06-16 09:33:50 -0700

    feat(core): add color grading schema and lut parsing

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -132,6 +132,28 @@ export {
 export { CSS_URL_RE, isNonRelativeUrl, isPathInside } from "./compiler/assetPaths";
 export { decodeUrlPathVariants } from "./utils/urlPath";
 export { parseAnimatedGifMetadata, type AnimatedGifMetadata } from "./media/gif";
+export {
+  HF_COLOR_GRADING_ATTR,
+  HF_COLOR_GRADING_ADJUST_KEYS,
+  HF_COLOR_GRADING_COLOR_SPACE,
+  HF_COLOR_GRADING_PRESETS,
+  getHfColorGradingPreset,
+  isHfColorGradingActive,
+  normalizeHfColorGrading,
+  normalizeHfColorGradingWithVariables,
+  parseHfColorGradingAttribute,
+  resolveHfColorGradingVariables,
+  serializeHfColorGrading,
+  type HfColorGrading,
+  type HfColorGradingAdjust,
+  type HfColorGradingAdjustKey,
+  type HfColorGradingLutRef,
+  type HfColorGradingPreset,
+  type HfColorGradingPresetId,
+  type HfColorGradingTarget,
+  type HfColorGradingVariableMap,
+  type NormalizedHfColorGrading,
+} from "./colorGrading";
 
 // Inline scripts
 export {
